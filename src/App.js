c188985a5950ed94fd1a55a077a12de7fad92d73
Peter Tardif <peter.tardif@gmail.com>
2019-05-30 00:28:02 -0600

    added API call and delete button"
    q
    "

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import Sidebar from './Sidebar/Sidebar';
 import NoteList from './NoteList/NoteList';
 import NoteListFilter from './NoteListFilter/NoteListFilter';
-import dummyStore from './dummy-store';
+// import dummyStore from './dummy-store';
 import NotePage from './NotePage/NotePage';
 import { NotesContext } from './NotesContext';
 import './App.css';
@@ -12,18 +12,49 @@ export default class App extends Component {
   state = {
     notes: [],
     folders: [],
+    error: null,
   }
 
   componentDidMount() {
     // fake date loading from API call
-    setTimeout(() => this.setState(dummyStore), 600)
+    // setTimeout(() => this.setState(dummyStore), 600)
+    Promise.all([
+      fetch(`http://localhost:9090/folders`),
+      fetch(`http://localhost:9090/notes`) 
+    ])
+      .then(([foldersResponse,notesResponse]) => {
+        if (!foldersResponse.ok) {
+          return foldersResponse.json().then(error => Promise.reject(error))
+        }
+        if (!notesResponse) {
+          return notesResponse.json().then(error => Promise.reject(error))
+        }
+        return Promise.all([
+          foldersResponse.json(),
+          notesResponse.json(),
+        ])
+      })
+      .then(([folders,notes]) => {
+        this.setState({ folders, notes})
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      })
   }
 
+  handleDeleteNote = noteId => {
+    this.setState({
+      notes: this.state.notes.filter(note => note.id !== noteId)
+    })
+  }
+  
   render() {
     const contextNotesValue = {
       notes: this.state.notes,
       addNote: this.addNote,
-      deleteNote: this.deleteNote,
+      deleteNote: this.handleDeleteNote,
       folders: this.state.folders,
       addFolder: this.addFolder,
       deleteFolder: this.deleteFolder,
--- src/Note/Note.js
+++ src/Note/Note.js
@@ -3,8 +3,36 @@ import { Link } from 'react-router-dom';
 import { NotesContext } from '../NotesContext';
 
 export default class Note extends Component {
+    static defaultProps = {
+        onDeleteNote: () => {},
+      }
+
     static contextType = NotesContext;
-    
+
+    handleClickDelete = event => {
+      event.preventDefault()
+      const noteId = this.props.i.id
+
+      fetch(`http://localhost:9090/notes/${noteId}`, {
+        method: 'DELETE',
+        headers: {
+            'content-type': 'application/json'
+        },
+    })
+        .then(response => {
+            if (!response.ok)
+                return response.json().then(error => Promise.reject(error))
+                    return response.json()
+        })
+        .then(() => {
+          this.context.deleteNote(noteId)
+          this.props.onDeleteNote(noteId)
+        })
+        .catch(error => {
+            console.error(error);
+        })
+    };
+
     render() {
       const noteContext = this.props.i;
       const date = new Date(noteContext.modified).toDateString();
@@ -12,7 +40,10 @@ export default class Note extends Component {
         <li key={noteContext.id}>
             <div>
                 <h2><Link to={`/note/${noteContext.id}`}>{noteContext.name}</Link></h2>
-                <button>Delete</button>
+                <button
+                    type='button'
+                    onClick={this.handleClickDelete}
+                >Delete</button>
                 <div>
                     Modified <span>{date}</span> 
                 </div>
